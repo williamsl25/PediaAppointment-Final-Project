cdbf2ad521a82369a7c05ac7c82e688030c18690
Lindsay Williams <williamsl25@yahoo.com>
2015-12-13 17:53:34 -0500

    added usercontroller and userservice

--- mobile/www/js/user/user.controller.js
+++ mobile/www/js/user/user.controller.js
@@ -4,7 +4,28 @@
   angular
   .module('users')
   .controller('UsersController', function ($scope, $stateParams, UsersService, $location){
+    UsersService.getPosts().success(function (posts) {
+        $scope.posts = posts;
+      });
 
-    })
+      if($routeParams.userId) {
+        UsersService.getSingleUser($routeParams.userId).success(function (singlePost) {
+          console.log(singleUser);
+          $scope.singleUser = singleUser;
+        });
+      }
+
+      $scope.newUser = function (user) {
+        UsersService.addUser(user);
+      };
+
+      $scope.editUser = function (editedUser) {
+        UsersService.updateUser(editedUser);
+      };
+      $scope.deleteUser = function (userId) {
+        UsersService.removeUser(userId);
+      };
+
+  });
 
 }());
