f96ae50678f39bd72688a3e61a0d34d6324b7426
Dustin Guthrie <guthrie.dustin@gmail.com>
2015-12-18 15:35:42 -0500

    all for you

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var profileRoutes = require('./routes/profile');
 var uploadRoutes = require('./routes/upload');
 var crudRoutes = require('./routes/crudRoutes');
 var role = require('./routes/roles');
+var cors = require('cors');
 
 mongoose.connect(config.MONGO_URI);
 mongoose.connection.on('error', function() {
@@ -24,18 +25,13 @@ app.use(role.middleware());
 app.use(methodOverride());
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+
 
 // Force HTTPS on Heroku
 if (app.get('env') === 'production') {
